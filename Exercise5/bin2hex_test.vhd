968518e5820f76dbe48a392cadb544d879e52b9f
GustavNK <gustav.knudsen96@gmail.com>
2020-03-18 09:21:46 +0100

    Opgave 5_1 lavet

---
+++ Exercise5/bin2hex_test.vhd
@@ -0,0 +1,19 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use work.all;
+
+entity bin2hex_test is
+port(
+	SW	: in std_logic_vector(3 downto 0);
+	HEX0	: out std_logic_vector(6 downto 0)
+);
+end;
+
+architecture bin2hex_test_arch of bin2hex_test is
+begin
+u1: entity bin2hex(bin2hex_arch)
+port map(
+	bin(3 downto 0) => SW(3 downto 0),
+	seg(6 downto 0) => HEX0(6 downto 0)
+);
+end;
