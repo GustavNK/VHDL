54419a8cd2e676bce4614f993b18a76ad19431bc
Christian-Bach-Johansen <1.christian.bach@gmail.com>
2020-04-20 11:17:08 +0200

    Fix i show_mux og waveform for nand8

--- Exercise5/two_player_show_mux.vhd
+++ Exercise5/two_player_show_mux.vhd
@@ -14,26 +14,27 @@ architecture two_player_show_mux_arch of two_player_show_mux is
 begin	
 		input_mux: process(show,set,input,try, player_input)
 	begin
-	if (player_input = '1') then
-					show1 <= show;
-				set1 	<= set;
-				try1 <= try;
-				output1 <= input;
-				--Default værdi til player0
-				show0 <= '1';
-				set0 	<= '1';
-				try0 <= '1';
-				output0 <= "00000000";
-	else
-				show0 <= show;
-				set0 	<= set;
-				try0 <= try;
-				output0 <= input;
-				--Default værdi
-				show1 <= '1';
-				set1 	<= '1';
-				try1 <= '1';
-				output1 <= "00000000";
-	end if;
+		case(player_input) is
+			when '1' =>
+				show1		<= show;
+				set1		<= set;
+				try1		<= try;
+				output1	<= input;
+				--Default vaerdi til player0
+				show0		<= '1';
+				set0		<= '1';
+				try0		<= '1';
+				output0	<= "00000000";
+		when others =>
+				show0		<= show;
+				set0		<= set;
+				try0		<= try;
+				output0	<= input;
+				--Default vaerdi
+				show1		<= '1';
+				set1		<= '1';
+				try1		<= '1';
+				output1	<= "00000000";
+	end case;
 	end process input_mux;
 end;
