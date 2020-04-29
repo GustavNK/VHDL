981221a3b5965218cf90a28a829c24cefeb928b3
GustavNK <gustav.knudsen96@gmail.com>
2020-04-29 11:31:42 +0200

    nej

--- Exercise7/UART_receiver.vhd
+++ Exercise7/UART_receiver.vhd
@@ -1,5 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity UART_receiver is
 port(
@@ -26,23 +27,22 @@ begin
 	end process;
 	
 	nxt_state: process(present_state, rxd)
-	variable bit_cnt	: natural;
+	variable bit_cnt	: integer range 0 to 8;
 	begin
 	next_state <= present_state; --default
 	case present_state is
 		when idle => 
 			if rxd = '0' then
 				next_state <= reading;
+				bit_cnt := 0;
 			end if;
 		when reading =>
 			bit_cnt := bit_cnt + 1;
 			if bit_cnt > 7 then
 				next_state <= stopping;
-			else
-				next_state <= reading;
 			end if;
 		when stopping =>
-			if rxd = '0' then
+			if rxd = '1' then
 				next_state <= latch_data;
 			else
 				next_state <= idle;
@@ -54,19 +54,19 @@ begin
 	end case;
 	end process;
 	
-	moore_out: process(present_state)
-	variable latch	: std_logic_vector(7 downto 0);
-	begin
-	case present_state is
-		when reading =>
-			latch := rxd & latch(7 downto 1);
-		when latch_data =>
-			rxdata <= latch;
-			rxvalid <= '1';
-		when idle =>
-			rxvalid <= '0';
-		when others =>
-			null;
-	end case;
-	end process;
+--	moore_out: process(present_state)
+--	variable latch	: std_logic_vector(7 downto 0);
+--	begin
+--	case present_state is
+--		when reading =>
+--			latch := rxd & latch(7 downto 1);
+--		when latch_data =>
+--			rxdata <= latch;
+--			rxvalid <= '1';
+--		when idle =>
+--			rxvalid <= '0';
+--		when others =>
+--			null;
+--	end case;
+--	end process;
 end;
